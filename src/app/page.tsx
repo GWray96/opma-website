84f2e306dd416e9ea7c155a29beae6df7e5f88ae
G4rfy <g4rfy@example.com>
2025-04-20 15:12:01 +0100

    remove: Delete VideoTestimonial section and component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import SingleTestimonial from '../components/testimonials/SingleTestimonial';
 import { featuredTestimonial } from '../components/testimonials/featuredTestimonial';
 import { secondTestimonial } from '../components/testimonials/secondTestimonial';
 import { thirdTestimonial } from '../components/testimonials/thirdTestimonial';
-import VideoTestimonial from '../components/testimonials/VideoTestimonial';
 import HomePainPoints from './home/home-components/home-pain-points';
 import HomeSolutions from './home/home-components/home-solutions';
 import HomeComparison from './home/home-components/home-comparison';
@@ -49,15 +48,6 @@ export default function Home() {
       
       {/* Comparison Section */}
       <HomeComparison />
-      
-      {/* Fourth Testimonial - Video */}
-      <div className="py-12 md:py-16 bg-gradient-to-b from-slate-900 to-purple-900/20">
-        <div className="container mx-auto px-4">
-          <div className="max-w-4xl mx-auto">
-            <VideoTestimonial videoUrl="https://youtube.com/shorts/TrQqcXaLJEE?feature=shared" />
-          </div>
-        </div>
-      </div>
     </div>
   );
 } 
